da9d60e0d7efe329ced04fc649f0790e9ea2a701
joemainer <mercywkiarie@gmail.com>
2023-04-25 11:29:10 -0700

    2pass

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,49 +1,46 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
- */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
-{
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
-}
-
-
-
-/**
- * insertion_sort_list - sorts a doubly linked list of
- * integers in ascending order
- * @list: A pointer to the head of the doubly linked list
- *
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ * order using an insertion sort algorithm
+ * @list: doubly linked list of integers to be sorted
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *lead, *follow, *new, *temp;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (!list || !(*list) || !((*list)->next))
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	/* dance begins with 1st from house left following */
+	follow = (*list);
+	/* and next dancer to house right leading */
+	lead = (*list)->next;
+	while (lead)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		new = lead->next;
+		while (follow && lead->n < follow->n)
 		{
-			swap_nodes(list, &insert, iter);
-			print_list((const listint_t *)*list);
+			/* lead and follow swap positions */
+			if (follow->prev)
+				follow->prev->next = lead;
+			else
+				/* if lead makes it to house left, now head */
+				*list = lead;
+			if (lead->next)
+				lead->next->prev = follow;
+			temp = lead->next;
+			lead->next = follow;
+			lead->prev = follow->prev;
+			follow->next = temp;
+			follow->prev = lead;
+			print_list(*list);
+			/* compare next pair, flowing to house left */
+			follow = lead->prev;
 		}
-
+		/* lead sorted to left, new cycle starts @ right leading edge */
+		lead = new;
+		if (lead)
+			follow = lead->prev;
 	}
+}
